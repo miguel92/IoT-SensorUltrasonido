aa873ffc31c85fb494b98f7b34d890a9e67f4b70
MiguelMartin <miguel92martin@gmail.com>
2021-05-16 17:50:14 +0200

    Daemon = True en el thread para que se mate cuando se cierra la app

--- main.py
+++ main.py
@@ -4,15 +4,14 @@ from iot_core import *
 import threading
 app = Flask(__name__)
 
-t = threading.Thread(target=main)
-t.start()
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
 if __name__ == '__main__':
+    t = threading.Thread(target=main, daemon=True)
+    t.start()
     app.run(host='127.0.0.1', port=8080, debug=True)
-    t.join()
-
 
