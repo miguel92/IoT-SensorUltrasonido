c8bbfa459e9dad7d3f59e811382c50684d996210
MiguelMartin <miguel92martin@gmail.com>
2021-05-17 19:13:05 +0200

    Llamada periodica a AJAX cada segundo para la recogida de la última lectura del sensor.

--- main.py
+++ main.py
@@ -3,13 +3,26 @@ from flask import Flask, render_template, request, redirect, url_for, session
 from bridge import *
 import threading
 from views import *
+import json
+from bson import json_util
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    datos=get_last_registry_view('455863')
-    return render_template('index.html',datos=datos)
+    return render_template('index.html')
+
+
+@app.route('/lectura_datos', methods=['POST','GET'])
+def lectura_datos():
+    response = {"estado": False, "datos": None}
+    datos = get_last_registry_view('455863')
+    if datos is not None:
+        response['estado'] = True
+        response['datos'] = list(datos)
+    response = json.dumps(response, default=json_util.default)
+    return response
 
 if __name__ == '__main__':
     '''
